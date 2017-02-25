41859ebe562cbbda9c262e71087f6ba96d4507c6
Lauren Arfaras <larfaras96@gmail.com>
2017-02-24 20:24:33 -0500

    added an api from typicode and google geocode and sent the data to the browser with certain parameters

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var server = express();
+var port = process.env.PORT || 8080;
+var axios = require("axios");
+var apiKey = require("./secrets").geocodeAPIKey;
+
+server.get("/posts/:postId", function(request, response) {
+  var url = `https://jsonplaceholder.typicode.com/posts/${request.params.postId}`;
+  axios.get(url)
+      .then(function(res){ // success
+        return res.data;
+      })
+      .then(function(data){
+        response.send(data);
+      })
+      .catch(function(error){ // failure
+        console.log(error);
+      })
+});
+
+server.get("/location/:address", function(request, response) {
+  // 1600+Amphitheatre+Parkway,+Mountain+View,+CA
+  var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.params.address}&key=${apiKey}`;
+  axios.get(url)
+      .then(function(res){ // success
+        return res.data;
+      })
+      .then(function(data){
+        response.send(data);
+      })
+      .catch(function(error){ // failure
+        console.log(error);
+      })
+})
+
+// server.get("/weather/:lat,:lon", function(request, response) {
+//   var url = `https://api.darksky.net/forecast/${apiKey}/${request.params.lat},${request.params.lon}`;
+//   axios.get(url)
+//       .then(function(res){ // success
+//         return res.data;
+//       })
+//       .then(function(data){
+//         response.send(data);
+//       })
+//       .catch(function(error){ // failure
+//         console.log(error);
+//       });
+// });
+
+server.listen(port, function(){
+  console.log("now listening on port ", port);
+});
---
+++ secrets.template.js
@@ -0,0 +1,4 @@
+// rename to secrets.js
+module.exports = {
+  geocodeAPIKey: "<<fill in your api key>>"
+}
